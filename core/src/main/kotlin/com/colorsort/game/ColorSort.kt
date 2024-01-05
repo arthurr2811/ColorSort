d034db23cc4b120123ed90de147650bb6d7113aa
YoungMovi <arthurraffel@gmail.com>
2024-01-05 15:45:46 +0100

    using camera now

--- core/src/main/kotlin/com/colorsort/game/ColorSort.kt
+++ core/src/main/kotlin/com/colorsort/game/ColorSort.kt
@@ -3,6 +3,7 @@ package com.colorsort.game
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
@@ -19,19 +20,20 @@ class ColorSort : ApplicationAdapter() {
     private val batch by lazy { SpriteBatch() }
     private val image by lazy { Texture("testBall.png") }
     private val ground by lazy { Texture("ground.png") }
+    private val camera : OrthographicCamera = OrthographicCamera()
     private lateinit var body : Body
     private lateinit var world : World
 
     override fun create() {
         // world
         world = World(Vector2(0f,-10f), true)
+        camera.setToOrtho(false, 400f, 800f)
         // ball
         val bodyDef = BodyDef()
         bodyDef.type = BodyDef.BodyType.DynamicBody
         // start position top middle
-        val positionX = Gdx.graphics.width / 2f
-        val totalHeight = Gdx.graphics.height
-        val positionY = totalHeight * 0.9f
+        val positionX = 400 / 2f
+        val positionY = 800 * 0.9f
         bodyDef.position.set(positionX, positionY)
         body = world.createBody(bodyDef)
         val fixtureDef = FixtureDef()
@@ -49,7 +51,7 @@ class ColorSort : ApplicationAdapter() {
         groundBodyDef.position.set(Vector2(0f,10f))
         val groundBody = world.createBody(groundBodyDef)
         val groundBox = PolygonShape()
-        groundBox.setAsBox(Gdx.graphics.width.toFloat(), Gdx.graphics.height / 2f)
+        groundBox.setAsBox(400f, 800 / 2f)
         groundBody.createFixture(groundBox, 0.0f)
         groundBox.dispose()
     }
@@ -59,9 +61,11 @@ class ColorSort : ApplicationAdapter() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         val x = body.position.x
         val y = body.position.y
+        camera.update()
+        batch.projectionMatrix = camera.combined
         batch.begin()
         batch.draw(image, x, y)
-        batch.draw(ground, Gdx.graphics.width/2f, Gdx.graphics.height / 2f)
+        batch.draw(ground, 400/2f, 800 / 2f)
         batch.end()
         // physics 2D testing
         world.step(1/60f,6,2)
@@ -70,5 +74,6 @@ class ColorSort : ApplicationAdapter() {
     override fun dispose() {
         batch.dispose()
         image.dispose()
+        ground.dispose()
     }
 }
